1688ebb4b1f431bf6ce13b3aa026ec277951abb8
spacegithub <space_star@yeah.net>
2018-06-12 19:19:57 +0800

    new add 环保处罚

--- src/main/java/com/mr/modules/api/site/instance/creditchinasite/mainsite/CreditChinaMainSite0002.java
+++ src/main/java/com/mr/modules/api/site/instance/creditchinasite/mainsite/CreditChinaMainSite0002.java
@@ -8,6 +8,11 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * @auther
  * 1.信用中国主站
@@ -33,6 +38,7 @@ public class CreditChinaMainSite0002 extends SiteTaskExtend_CreditChina{
      * 获取网页内容
      */
     public void extractContext(String url){
+        List<Map<String,String>> listPersonObjectMap = new ArrayList<>();
         //来源
         String source = "信用中国";
         //来源地址
@@ -45,11 +51,11 @@ public class CreditChinaMainSite0002 extends SiteTaskExtend_CreditChina{
         // 资质证号、
         String aptitudeNo = "";
         // 惩罚时间、
-        String punishDate = "";
+        String punishDate = "2018年3月26日";
         // 奖惩部门、
         String executeOrg = "";
         // 惩罚类型、
-        String punishType = "环保部公布的环评机构不良行为记录";
+        String punishType = "受到环保部门两次及以上行政处理的环评工程师名单记录";
         // 惩罚原因
         String punishReason = "";
         //行政处理方式
@@ -57,15 +63,61 @@ public class CreditChinaMainSite0002 extends SiteTaskExtend_CreditChina{
 
         Document document  = Jsoup.parse(getHtmlPage(url,1000));
         Element elementTable = document.getElementsByTag("table").first();
-        Elements elementTr = elementTable.getElementsByTag("tr");
-        for(Element elementTd : elementTr){
-            Elements elementsTdList = elementTd.getElementsByTag("td");
-            if(elementsTdList.size()==7){
-                int count = Integer.valueOf(elementsTdList.get(6).text().trim());
-                for(int i =0 ; i<count;i++){
-
+        Elements elementTrs = elementTable.getElementsByTag("tr");
+        for(int i =0;i<elementTrs.size();i++){
+            if(i>0){
+                Elements elementsTdList = elementTrs.get(i).getElementsByTag("td");
+                if(elementsTdList.size() ==7){
+                    punishNo = elementsTdList.get(2).text();
+                    aptitudeNo = elementsTdList.get(2).text();
+                    environDiscussPerson = elementsTdList.get(1).text();
+                    // 奖惩部门、String executeOrg = "";
+                    executeOrg = elementsTdList.get(3).text();
+                    punishReason = elementsTdList.get(4).text();
+                    punishMethod = elementsTdList.get(5).text();
+                }else{
+                    executeOrg = elementsTdList.get(0).text();
+                    punishReason = elementsTdList.get(1).text();
+                    punishMethod = elementsTdList.get(2).text();
                 }
+                //int count = Integer.valueOf(elementsTdList.get(6).text().trim());
+                Map<String,String> personObjectMap = new HashMap<>();
+                //来源 String source = "信用中国";
+                personObjectMap.put("source",source);
+                //来源地址 String sourceUrl = url;
+                personObjectMap.put("sourceUrl",sourceUrl);
+                //处理文号 String punishNo = "";
+                personObjectMap.put("punishNo",punishNo);
+                //姓名、String environDiscussPerson = "";
+                personObjectMap.put("environDiscussPerson",environDiscussPerson);
+                // 资质证号、String aptitudeNo = "";
+                personObjectMap.put("aptitudeNo",aptitudeNo);
+                // 惩罚时间、String punishDate = "";
+                personObjectMap.put("punishDate",punishDate);
+                // 奖惩部门、String executeOrg = "";
+                personObjectMap.put("executeOrg",executeOrg);
+                // 惩罚类型、String punishType = "环保部公布的环评机构不良行为记录";
+                personObjectMap.put("punishType",punishType);
+                // 惩罚原因 String punishReason = "";
+                personObjectMap.put("punishReason",punishReason);
+                //行政处理方式 String punishMethod = "";
+                personObjectMap.put("punishMethod",punishMethod);
+                listPersonObjectMap.add(personObjectMap);
             }
+        }
+        for(Map<String,String> map : listPersonObjectMap){
+            log.info(
+                    "来源："+map.get("source")+"\n"
+                            +"来源地址："+map.get("sourceUrl")+"\n"
+                            +"处罚证书："+ map.get("punishNo")+"\n"
+                            +"姓名："+ map.get("environDiscussPerson")+"\n"
+                            +"资质证号："+  map.get("aptitudeNo")+"\n"
+                            +"惩罚时间："+  map.get("punishDate")+"\n"
+                            +"奖惩部门："+  map.get("executeOrg")+"\n"
+                            +"惩罚类型："+  map.get("punishType")+"\n"
+                            +"惩罚原因："+  map.get("punishReason")+"\n"
+                            +"行政处理方式："+  map.get("punishMethod")+"\n" );
+            log.info("-----------------------------------------------------------------------------------");
         }
         log.info(document.text());
     }
--- src/main/java/com/mr/modules/api/site/instance/creditchinasite/mainsite/CreditChinaMainSite0003.java
+++ src/main/java/com/mr/modules/api/site/instance/creditchinasite/mainsite/CreditChinaMainSite0003.java
@@ -1,5 +1,24 @@
 package com.mr.modules.api.site.instance.creditchinasite.mainsite;
 
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.Page;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.mr.common.OCRUtil;
+import com.mr.common.util.SpringUtils;
+import com.mr.modules.api.site.SiteTaskExtend_CreditChina;
+import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * @auther
  * 1.信用中国主站
@@ -7,6 +26,138 @@ package com.mr.modules.api.site.instance.creditchinasite.mainsite;
  * 3.需求：2017 年环境违法企业“黑名单”
  * 4.提取内容：企业名称、统一社会信用代码（或组织机构代码或工商注册号）、法定代表人/实际经营者姓名、详细地址、违法情形、日期
  */
+@Slf4j
+@Component("creditChinaMainSite0003")
+@Scope("prototype")
+public class CreditChinaMainSite0003 extends SiteTaskExtend_CreditChina {
+    protected OCRUtil ocrUtil = SpringUtils.getBean(OCRUtil.class);
 
-public class CreditChinaMainSite0003 {
+
+    String url ="https://www.creditchina.gov.cn/xinxigongshi/huanbaolingyu/201804/t20180425_114081.html";
+    @Override
+    protected String executeOne() throws Throwable {
+        return super.executeOne();
+    }
+
+    @Override
+    protected String execute() throws Throwable {
+        extractContext(url);
+        return null;
+    }
+    /**
+     * 获取网页内容
+     */
+    public void extractContext(String url) throws Throwable{
+        List<Map<String, String>> listPersonObjectMap = new ArrayList<>();
+        //来源
+        String source = "信用中国";
+        //来源地址
+        String sourceUrl = url;
+        //企业名称、
+        String commpanyName = "";
+        // 统一社会信用代码（或组织机构代码或工商注册号）、
+        String nnifiedSocialCreditCode = "";
+        // 法定代表人/实际经营者姓名、
+        String legalRepresentative = "";
+        // 详细地址、
+        String detailAddress = "";
+        // 违法情形、
+        String transgress = "";
+        // 日期
+        String dateString = "2018年1月16日";
+        Document document  = Jsoup.parse(getHtmlPage(url,1000));
+        Element element = document.getElementsByClass("TRS_Editor").first();
+        Element elementA_PDF = element.getElementsByTag("a").first();
+        String  fName = elementA_PDF.attr("href").substring(elementA_PDF.attr("href").lastIndexOf("/")+1);
+        WebClient wc = new  WebClient(BrowserVersion.CHROME);
+        wc.getOptions().setUseInsecureSSL(true);
+        String pagePDFUrl = "https://www.creditchina.gov.cn/xinxigongshi/huanbaolingyu/201804/"+elementA_PDF.attr("href").replace("./","");
+        Page pagePDF =wc.getPage(pagePDFUrl);
+        String fileName = saveFile(pagePDF,fName);
+        String pdfString = "";
+        //重试次数
+        int resetCount = 1;
+        System.out.println("fileName-----------------------\n"+fileName);
+        try{
+            pdfString = ocrUtil.getTextFromPdf(fileName);
+        }catch (Exception e){
+
+            while (resetCount==1){
+                pdfString =  ocrUtil.getTextFromPdf(fileName);
+                resetCount++;
+            }
+
+        }
+        pdfString = pdfString.replaceAll("(\\r\\n|\\r|\\n|\\n\\r)","");
+        pdfString = pdfString.replaceAll("附件 2017年环境违法企业“黑名单” ","");
+        pdfString = pdfString.replaceAll("序号 企业名称 组织机构代码/统一社会信用代码/工商登记注册号 法定代表人/实际经营者姓名 详细地址 环境违法情形 ","").trim();
+        String[] strPdf = pdfString.split(" ");
+
+        for(int i = 0 ;i<strPdf.length;i++){
+            int countFlag = strPdf.length%6;
+
+
+            if(i%6==1){
+                //企业名称、String commpanyName = "";
+                commpanyName = strPdf[i];
+
+            }
+            if(i%6==2){
+                // 统一社会信用代码（或组织机构代码或工商注册号）、String nnifiedSocialCreditCode = "";
+                nnifiedSocialCreditCode = strPdf[i];
+
+            }
+            if(i%6==3){
+                // 法定代表人/实际经营者姓名、String legalRepresentative = "";
+                legalRepresentative = strPdf[i];
+
+            }
+            if(i%6==4){
+                // 详细地址、String detailAddress = "";
+                detailAddress = strPdf[i];
+
+            }
+            if(i%6==4){
+                // 违法情形、String transgress = "";
+                transgress = strPdf[i];
+
+            }
+            if(countFlag==0){
+                Map<String,String> personObjectMap  = new HashMap<>();
+                //来源String source = "信用中国";
+                personObjectMap.put("source",source);
+                //来源地址String sourceUrl = url;
+                personObjectMap.put("sourceUrl",sourceUrl);
+                // 日期String dateString = "";
+                personObjectMap.put("dateString",dateString);
+                personObjectMap.put("commpanyName",commpanyName);
+                personObjectMap.put("nnifiedSocialCreditCode",nnifiedSocialCreditCode);
+                personObjectMap.put("legalRepresentative",legalRepresentative);
+                personObjectMap.put("detailAddress",detailAddress);
+                personObjectMap.put("transgress",transgress);
+
+                listPersonObjectMap.add(personObjectMap);
+            }
+        }
+
+        //System.out.println("-----------------------\n"+pdfString);
+        for(Map<String,String> map : listPersonObjectMap){
+            log.info(
+                    "\n来源："+map.get("source") +
+                    "\n来源地址："+map.get("sourceUrl") +
+                    "\n日期："+map.get("dateString")+
+                    "\n企业名称："+map.get("commpanyName")+
+                    "\n统一社会信用代码："+map.get("nnifiedSocialCreditCode")+
+                    "\n法定代表人："+map.get("legalRepresentative")+
+                    "\n详细地址："+map.get("detailAddress")+
+                    "\n违法情形："+map.get("transgress")
+            );
+
+        }
+
+
+
+    }
+
 }
+

7da46aa1116da884529f5e13e353a829f625f001
spacegithub <space_star@yeah.net>
2018-07-05 16:29:36 +0800

    机构名称中带有括号，将其与其中的内容去掉

--- src/main/java/com/mr/modules/api/site/SiteTaskExtendSub.java
+++ src/main/java/com/mr/modules/api/site/SiteTaskExtendSub.java
@@ -123,6 +123,9 @@ public abstract class SiteTaskExtendSub extends SiteTaskExtend {
 	 */
 	public String delFinallyString(String strOld,String delString){
 		String strNew = strOld;
+		if(strOld.length()>0&&strOld.trim().endsWith(delString)){
+			strNew = strOld.substring(0,strOld.length()-1);
+		}
 		//此必须要放在第一个位置
 		if(strNew.contains("披露（")&&!strNew.contains("）")){
 			strNew = strNew.replaceAll(".*披露（","");
@@ -131,12 +134,33 @@ public abstract class SiteTaskExtendSub extends SiteTaskExtend {
 		if(strNew.contains("（")&&!strNew.contains("）")){
 			strNew = strNew.replaceAll("（.*","");
 		}
-		//str ="人保财险潜山支公（司（dsfdsfsdfsdd）水）电费水电费的";
-		if(strNew.contains("（")&&strNew.contains("）")){
-			strNew = strNew.replaceAll("（.*）","");
+		//str ="人保财险潜山支公（以下简称）电费水电费的";
+		if(strNew.contains("（以下简称")&&strNew.contains("）")){
+			strNew = strNew.replaceAll("（以下简称.*）","");
 		}
-		if(strOld.length()>0&&strOld.trim().endsWith(delString)){
-			strNew = strOld.substring(0,strOld.length()-1);
+		//str ="人保财险潜山支公[原名XXXX]费水电费的";
+		if(strNew.contains("[原名")&&strNew.contains("]")){
+			strNew = strNew.replaceAll("\\[原名.*\\]","");
+		}
+		//str ="人保财险潜山支公（下简称XXXX）费水电费的";
+		if(strNew.contains("（下简称")&&strNew.contains("）")){
+			strNew = strNew.replaceAll("（下简称.*）","");
+		}
+		//str ="人保财险潜山支公（原名XXXX）费水电费的";
+		if(strNew.contains("（原")&&strNew.contains("）")){
+			strNew = strNew.replaceAll("（原.*）","");
+		}
+		//str ="人保财险潜山支公（已更名XXXX）费水电费的";
+		if(strNew.contains("（已更名")&&strNew.contains("）")){
+			strNew = strNew.replaceAll("（已更名.*）","");
+		}
+		//str ="人保财险潜山支公（简称XXXX）费水电费的";
+		if(strNew.contains("（简称")&&strNew.contains("）")){
+			strNew = strNew.replaceAll("（简称.*）","");
+		}
+		//str ="人保财险潜山支公（该公司2011年5月公司登记名称为XXXX）费水电费的";
+		if(strNew.contains("（该公司2011年5月公司登记名称为")&&strNew.contains("）")){
+			strNew = strNew.replaceAll("（该公司2011年5月公司登记名称为.*）","");
 		}
 		return strNew.toString();
 	}

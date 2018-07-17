7792e95101be21ad45934a4f43e538ed771214d5
spacegithub <space_star@yeah.net>
2018-07-17 18:45:07 +0800

    bug修复

--- src/main/java/com/mr/modules/api/site/SiteTaskExtend_CreditChina.java
+++ src/main/java/com/mr/modules/api/site/SiteTaskExtend_CreditChina.java
@@ -388,6 +388,9 @@ public class SiteTaskExtend_CreditChina extends SiteTaskExtend{
          */
         //@Column(name = "enterprise_code3")
         discreditBlacklist.setEnterpriseCode3(map.get("enterpriseCode3")==null?"":map.get("enterpriseCode3").toString());
+
+        //enterprise_code4	纳税人识别号
+        discreditBlacklist.setEnterpriseCode3(map.get("enterpriseCode4")==null?"":map.get("enterpriseCode4").toString());
         /**
          * 法定代表人/负责人姓名|负责人姓名
          */

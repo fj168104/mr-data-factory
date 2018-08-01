67d82b4e1410d0e3b56547583af341ea21659c03
spacegithub <space_star@yeah.net>
2018-08-01 12:03:18 +0800

    爬虫第三期目录构建，类构建，TODO待实现

---
+++ src/main/java/com/mr/modules/api/site/instance/colligationsite/mofcomsite/MOFCOM_SXBG.java
@@ -0,0 +1,12 @@
+package com.mr.modules.api.site.instance.colligationsite.mofcomsite;
+
+/**
+ * 站点：国家商务部网站
+ * url：http://www.ipraction.gov.cn/article/xxgk/shxy/sxbg/
+ * 主题：失信曝光
+ * 属性：企业名称 检查产品  检查结果 检查机关  发布日期
+ * 提取：TODO 公司名称 发布日期  检查机关
+ */
+
+public class MOFCOM_SXBG {
+}
